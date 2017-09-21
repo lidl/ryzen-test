72d7341de53980d299e648042f80b9990d76f70a
Kurt Lidl <lidl@pix.net>
2017-09-21 15:35:27 -0400

    Rewrite scripts to use functions and not rely on bash features
    Changed order of args for the buildloop.sh script, so that the
    number of jobs to run comes first, and the loop name comes second.
    
    Output of the loop/pass information should be the same, some of
    the messages before that have been altered for clearer meaning.

--- buildloop.sh
+++ buildloop.sh
@@ -1,20 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-function error() {
-  echo $(date)" ${1} failed"
-  exit 1
+error() {
+	echo $(date)" ${1} failed"
+	exit 1
 }
 
-NAME="$1"
-TPROC="$2"
-CDIR="$PWD"
+JOBS="$1"
+NAME="$2"
+
+CDIR="$(pwd)"
 WDIR="${CDIR}/buildloop.d/${NAME}/"
-for ((I=0;1;I++)); do
-  cd "${CDIR}" || error "leave workdir"
-  echo $(date)" start ${I}"
-  [ -e "${WDIR}" ] && rm -rf "${WDIR}"
-  mkdir -p "${WDIR}" || error "create workdir"
-  cd "${WDIR}" || error "change to workdir"
-  ${CDIR}/gcc-7.1.0/configure --disable-multilib &> configure.log || error "configure"
-  gmake -j "$TPROC" &> build.log || error "build"
+
+PASS=0
+while :
+do
+	cd "${CDIR}" || error "leave workdir"
+	echo $(date)" start ${PASS}"
+	[ -e "${WDIR}" ] && rm -rf "${WDIR}"
+	mkdir -p "${WDIR}" || error "create workdir"
+	cd "${WDIR}" || error "change to workdir"
+	${CDIR}/gcc-7.1.0/configure --disable-multilib > configure.log 2>&1 || \
+		error "configure"
+	gmake -j "$JOBS" > build.log 2>&1 || error "build"
+	PASS=$((PASS + 1))
 done
--- kill-ryzen.sh
+++ kill-ryzen.sh
@@ -1,96 +1,169 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+# globals
 export LANG=C
+UNAME=$(uname)
+CLEAN_ON_EXIT=false # must be 'true' or 'false'
+GCC_FILE=gcc-7.1.0.tar.bz2
+export GCCDIR="$(pwd)"
+BSCRIPT=buildloop.sh
 
-USE_RAMDISK=false
-USE_TMPFS=true
-CLEAN_ON_EXIT=false
-NPROC=$1
-TPROC=$2
+# figure out how many CPUs to use, if not specified
+set_NPROC() {
+	case $UNAME in
+	FreeBSD)
+		NPROC=$(sysctl -n kern.smp.cpus)
+		CPUINFO=$(sysctl -n hw.model)
+		;;
+	Linux)
+		which nproc && :
+		if [ $? -eq 0 ]; then
+			NPROC=$(nproc)
+		else
+			NPROC=$(awk '/cpu cores/ {print $4}' < /proc/cpuinfo)
+		fi
+		CPUINFO=$(egrep "^model name" /proc/cpuinfo)
+		;;
+	esac
+}
 
-[ -n "$NPROC" ] || NPROC=$(nproc)
-[ -n "$TPROC" ] || TPROC=1
+# mount a ram based filesystem for testing
+mount_workspace() {
+	local mountpoint workdir
+	case $UNAME in
+	FreeBSD)
+		mountpoint=$(pwd)/workdir
+		workdir=workdir
+		mkdir -p $workdir || exit 1
+		if ( mount | grep -q $workdir ); then
+			sudo umount $workdir
+		fi
+		sudo mount -t tmpfs tmpfs $mountpoint || exit 1
+		;;
+	Linux)
+		echo "Create compressed ramdisk"
+		mountpoint=/mnt/ramdisk
+		workdir=$mountpoint/workdir
+		set -e
+		sudo mkdir -p $mountpoint
+		sudo modprobe zram num_devices=1
+		echo 64G | sudo tee /sys/block/zram0/disksize
+		sudo mkfs.ext4 -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
+		sudo mount -o relatime,nosuid,discard /dev/zram0 $mountpoint
+
+		sudo mkdir -p $workdir
+		sudo chmod 777 $workdir
+
+		set -x
+		;;
+	esac
+	# $mountpoint and $workdir should be set at this point
+	cp ${BSCRIPT} $workdir/${BSCRIPT}
+	cd $workdir
+	mkdir tmpdir || exit 1
+
+	# set some more environmental variables
+	export TMPDIR="$(pwd)/tmpdir"
+	export MOUNTPOINT=$mountpoint
+}
 
 cleanup() {
-  if $USE_RAMDISK; then
-    sudo rm -rf /mnt/ramdisk/*
-    sudo umount /mnt/ramdisk
-  elif $USE_TMPFS; then
-    cd ..
-    sudo umount workdir
-  fi
+	case $UNAME in
+	Linux)
+		sudo rm -rf $MOUNTPOINT/*
+		;;
+	esac
+	sudo umount $MOUNTPOINT
 }
-if $CLEAN_ON_EXIT; then
-  trap "cleanup" SIGHUP SIGINT SIGTERM EXIT
-fi
 
-echo "Install required packages"
-if which apt-get &>/dev/null; then
- sudo apt-get install build-essential
-elif which dnf &>/dev/null; then
- sudo dnf install -y @development-tools
-elif which pkg &>/dev/null; then
- sudo pkg install mpfr mpc gmp
-else
- exit 1
-fi
+install_packages() {
+	echo "Installing required packages..."
+	if which apt-get >/dev/null 2>&1; then
+		sudo apt-get install build-essential
+	elif which dnf >/dev/null 2>&1; then
+		sudo dnf install -y @development-tools
+	elif which pkg >/dev/null 2>&1; then
+		# only attempt to install if not present
+		for pkg in mpfr mpc gmp
+		do
+			pkg info -q $pkg || sudo pkg install $pkg
+		done
+	else
+		exit 1
+	fi
+}
 
-echo "Download GCC sources"
-[ -f gcc-7.1.0.tar.bz2 ] || wget https://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2 || exit 1
-export GCCDIR="$PWD"
+download_gcc() {
+	local url=https://ftp.gnu.org/gnu/gcc/gcc-7.1.0/${GCC_FILE}
+	if [ -f ${GCC_FILE} ]; then
+		return
+	fi
+	echo "Downloading GCC source file"
+	if which wget >/dev/null 2>&1; then
+		wget ${url} || exit 1
+	elif which fetch >/dev/null 2>&1; then
+		fetch ${url} || exit 1
+	elif which curl >/dev/null 2>&1; then
+		curl -O ${url} || exit 1
+	else
+		exit 1
+	fi
+}
 
-if $USE_RAMDISK; then
-  echo "Create compressed ramdisk"
-  sudo mkdir -p /mnt/ramdisk || exit 1
-  sudo modprobe zram num_devices=1 || exit 1
-  echo 64G | sudo tee /sys/block/zram0/disksize || exit 1
-  sudo mkfs.ext4 -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0 || exit 1
-  sudo mount -o relatime,nosuid,discard /dev/zram0 /mnt/ramdisk/ || exit 1
-  sudo mkdir -p /mnt/ramdisk/workdir || exit 1
-  sudo chmod 777 /mnt/ramdisk/workdir || exit 1
-  cp buildloop.sh /mnt/ramdisk/workdir/buildloop.sh || exit 1
-  cd /mnt/ramdisk/workdir || exit 1
-  mkdir tmpdir || exit 1
-  export TMPDIR="$PWD/tmpdir"
-elif $USE_TMPFS; then
-  mkdir -p workdir || exit 1
-  if ( mount | grep -q workdir ); then
-    sudo umount workdir
-  fi
-  sudo mount -t tmpfs tmpfs workdir || exit 1
-  cp buildloop.sh workdir/ || exit 1
-  cd workdir || exit 1
-  mkdir tmpdir || exit 1
-  export TMPDIR="$PWD/tmpdir"
-fi
+show_machine_info() {
+	echo "sudo dmidecode -t memory | grep -i -E \"(rank|speed|part)\" | grep -v -i unknown"
+	sudo dmidecode -t memory | grep -i -E "(rank|speed|part)" | grep -v -i unknown
+	echo "uname -a"
+	uname -a
+	echo $CPUINFO
+}
 
-echo "Extract GCC sources"
-tar xf "$GCCDIR/gcc-7.1.0.tar.bz2" || exit 1
+os_kludges() {
+	case $UNAME in
+	FreeBSD)
+		;;
+	Linux)
+		# start journal process in different working directory
+		pushd /
+		journalctl -kf | sed 's/^/[KERN] /' &
+		popd
+		;;
+	esac
+}
 
-echo "Download prerequisites"
-#(cd gcc-7.1.0/ && ./contrib/download_prerequisites)
+main() {
+	NPROC=$1
+	JOBS=$2
 
-[ -d 'buildloop.d' ] && rm -r 'buildloop.d'
-mkdir -p buildloop.d || exit 1
+	[ -n "$NPROC" ] || set_NPROC
+	[ -n "$JOBS" ] || JOBS=1
 
-#echo "cat /proc/cpuinfo | grep -i -E \"(model name|microcode)\""
-#cat /proc/cpuinfo | grep -i -E "(model name|microcode)"
-echo "sudo dmidecode -t memory | grep -i -E \"(rank|speed|part)\" | grep -v -i unknown"
-sudo dmidecode -t memory | grep -i -E "(rank|speed|part)" | grep -v -i unknown
-echo "uname -a"
-uname -a
-#echo "cat /proc/sys/kernel/randomize_va_space"
-#cat /proc/sys/kernel/randomize_va_space
+	$CLEAN_ON_EXIT && trap "cleanup" SIGHUP SIGINT SIGTERM EXIT
 
-# start journal process in different working directory
-#pushd /
-#  journalctl -kf | sed 's/^/[KERN] /' &
-#popd
-echo "Using ${NPROC} parallel processes"
+	download_gcc
+	install_packages
+	mount_workspace
 
-START=$(date +%s)
-for ((I=0;$I<$NPROC;I++)); do
-  (./buildloop.sh "loop-$I" "$TPROC" || echo "TIME TO FAIL: $(($(date +%s)-${START})) s") | sed "s/^/\[loop-${I}\] /" &
-  sleep 1
-done
+	echo -n "Extracting GCC sources..."
+	tar xf "$GCCDIR/gcc-7.1.0.tar.bz2" || exit 1
+	echo " done."
 
-wait
+	[ -d 'buildloop.d' ] && rm -r 'buildloop.d'
+	mkdir -p buildloop.d || exit 1
+
+	show_machine_info
+	os_kludges
+
+	echo "Using ${NPROC} parallel processes (JOBS: $JOBS)"
+
+	START=$(date +%s)
+	I=0
+	while [ $I -lt $NPROC ]; do
+		I=$((I + 1))
+		(./${BSCRIPT} "$JOBS" "loop-$I" || echo "TIME TO FAIL: $(($(date +%s)-${START})) s") | sed "s/^/\[loop-${I}\] /" &
+		sleep 1
+	done
+	wait
+}
+
+main "$@"
